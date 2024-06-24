ea921735e1f2771cadf362195e392ec687980070
Hitesh Natha <hiteshnatha15@gmail.com>
2024-06-24 23:36:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Summed Matrix/summed-matrix.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    long long sumMatrix(long long n, long long q) {
+        // code here
+        if(q<2||q>2*n){
+            return 0;
+        }
+         long long centre=1+n;
+         long long DistFromCentre=abs(centre-q);
+         return n-DistFromCentre;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long n,q;
+        
+        cin>>n>>q;
+
+        Solution ob;
+        cout << ob.sumMatrix(n,q) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
