22b1851d31cfeb2eb49baad2ea91bf417cb8e15e
Hitesh Natha <hiteshnatha15@gmail.com>
2024-07-18 13:16:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest alternating subsequence/longest-alternating-subsequence.cpp
@@ -0,0 +1,80 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int alternatingMaxLength(vector<int>& arr) {
+        // Code here
+           int n=arr.size();
+        int cnt=1;
+        int ans=1;
+        int num=arr[0];
+        int inc=1;
+        for(int i=1;i<n;i++)
+        {
+            if(arr[i]>num && inc)
+            {
+                
+                inc=0;
+                cnt++;
+                ans=max(ans,cnt);
+            }
+           
+            else if(arr[i]<num && !inc)
+            {
+                
+                inc=1;
+                cnt++;
+                ans=max(ans,cnt);
+            }
+            num=arr[i];
+        }
+        cnt=1;
+        num=arr[0];
+        inc=0;
+        for(int i=1;i<n;i++)
+        {
+            if(arr[i]<num && !inc)
+            {
+                
+                inc=1;
+                cnt++;
+                ans=max(ans,cnt);
+            }
+            else if(arr[i]>num && inc)
+            {
+                
+                inc=0;
+                cnt++;
+                ans=max(ans,cnt);
+            }
+            num=arr[i];
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    cin.ignore();
+    while (tc--) {
+        string s;
+        getline(cin, s);
+        stringstream ss(s);
+        vector<int> arr;
+        string temp;
+        while (ss >> temp) {
+            arr.push_back(stoi(temp));
+        }
+        Solution obj;
+        int ans = obj.alternatingMaxLength(arr);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
