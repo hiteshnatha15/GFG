d47645746f49c622a6a511683fbf72f529519211
Hitesh Natha <hiteshnatha15@gmail.com>
2024-07-21 17:56:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Sub-Array with Sum K/longest-sub-array-with-sum-k.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    int lenOfLongSubarr(int A[],  int N, int K) 
+    { 
+        // Complete the function
+        map<int,int> mp;
+        int sum=0;
+        int maxLen=0;
+        for(int i=0;i<N;i++)
+        {
+            sum=sum+A[i];
+            if(sum==K)
+            {
+                maxLen=max(maxLen,i+1);
+            }
+            int rem=sum-K;
+            if(mp.find(rem)!=mp.end())
+            {
+                maxLen=max(maxLen,i-mp[rem]);
+            }
+            if(mp.find(sum)==mp.end())
+            {
+                mp[sum]=i;
+            }
+        }
+        return maxLen;
+    } 
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	//code
+	
+	int t;cin>>t;
+	while(t--)
+	{
+	    int n, k;
+	    cin>> n >> k;
+	    int a[n];
+	    
+	    for(int i=0;i<n;i++)
+	        cin>>a[i];
+	   Solution ob;
+	   cout << ob.lenOfLongSubarr(a, n , k)<< endl;
+	    
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
