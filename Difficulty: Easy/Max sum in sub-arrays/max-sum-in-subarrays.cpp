127e81d3cc79015e1288be910e21eee813319ade
Hitesh Natha <hiteshnatha15@gmail.com>
2024-07-25 22:45:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Max sum in sub-arrays/max-sum-in-subarrays.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+  public:
+    // Function to find pair with maximum sum
+    int pairWithMaxSum(vector<int> &arr) {
+        // Your code goes here
+        int maxs=INT_MIN;
+        int sum=0;
+        for(int i=0;i<arr.size()-1;i++){
+            sum=arr[i+1]+arr[i];
+            maxs=max(sum,maxs);
+        }
+        return maxs;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+
+    while (t--) {
+        string line;
+        getline(cin, line);
+        stringstream ss(line);
+        vector<int> nums;
+        int num;
+        while (ss >> num) {
+            nums.push_back(num);
+        }
+        Solution ob;
+        cout << ob.pairWithMaxSum(nums) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
