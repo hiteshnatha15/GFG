436ba611ef1de7328db4c2b2604389df62a24676
Hitesh Natha <hiteshnatha15@gmail.com>
2024-07-29 16:27:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Median in a row-wise sorted Matrix/median-in-a-rowwise-sorted-matrix.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    int median(vector<vector<int>> &a, int R, int C){
+        // code here       
+        int min=1,max=2000;
+           
+           int des=(R*C+1)/2;
+           while(min<max){
+               int mid=(min+max)/2;
+               int place=0;
+               for(int i=0;i<R;i++)
+               place+=upper_bound(a[i].begin(),a[i].end(),mid)-a[i].begin();
+                
+                if(place<des)min=mid+1;
+                else max=mid;
+               
+           }
+           return min;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int r, c;
+        cin>>r>>c;
+        vector<vector<int>> matrix(r, vector<int>(c));
+        for(int i = 0; i < r; ++i)
+            for(int j = 0;j < c; ++j)
+                cin>>matrix[i][j];
+        Solution obj;
+        int ans=-1;
+        ans=obj.median(matrix, r, c);
+        cout<<ans<<"\n";        
+    }
+    return 0;
+}
+// } Driver Code Ends
