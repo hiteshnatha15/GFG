b144f3049cc0a513ac0368336c4cb0fc3d18de74
Hitesh Natha <hiteshnatha15@gmail.com>
2024-07-31 20:52:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Union of Two Sorted Arrays/union-of-two-sorted-arrays.cpp
+++ Difficulty: Medium/Union of Two Sorted Arrays/union-of-two-sorted-arrays.cpp
@@ -11,33 +11,35 @@ class Solution{
     //Function to return a list containing the union of the two arrays. 
     vector<int> findUnion(int arr1[], int arr2[], int n, int m)
     {
-        //Your code here
-        //return vector with correct order of elements
         int i=0;
         int j=0;
         vector<int> ans;
-        while(i<n && j<m) {
+        while(i<n && j<m){
             if(arr1[i]<=arr2[j]){
-                if(ans.size() == 0 || ans.back()!=arr1[i])
+                if(ans.size()==0 || ans.back()!=arr1[i]){
                     ans.push_back(arr1[i]);
-                i++;
+                    }
+                    i++;
             }
-            else{
-                if(ans.size() == 0 || ans.back()!=arr2[j])
+                else{
+                    if(ans.size()==0 || ans.back()!=arr2[j]){
                     ans.push_back(arr2[j]);
-                j++;
-            }
+                    }
+                    j++;
+                }
         }
         while(i<n){
-                if(ans.empty() || ans.back()!=arr1[i])
-                    ans.push_back(arr1[i]);
-                i++;
+            if(ans.size()==0 || ans.back()!=arr1[i]){
+                ans.push_back(arr1[i]);
             }
-            while(j<m){
-                if(ans.empty() || ans.back()!=arr2[j])
-                    ans.push_back(arr2[j]);
-                j++;
+            i++;
+        }
+        while(j<m){
+            if(ans.size()==0 || ans.back()!=arr2[j]){
+                ans.push_back(arr2[j]);
             }
+            j++;
+        }
         return ans;
     }
 };
