50dcc96fbe75db093db2513ece644a230c0f45bb
Hitesh Natha <hiteshnatha15@gmail.com>
2024-08-28 11:03:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sorting Elements of an Array by Frequency/sorting-elements-of-an-array-by-frequency.cpp
+++ Difficulty: Medium/Sorting Elements of an Array by Frequency/sorting-elements-of-an-array-by-frequency.cpp
@@ -3,68 +3,65 @@
 using namespace std;
 
 
-
 // } Driver Code Ends
-class Solution{
-    public:
-    //Complete this function
-    //Function to sort the array according to frequency of elements.
-    vector<int> sortByFreq(int arr[],int n)
-    {
-        //Your code here
-        vector<int> v;
-        map<int,int> mp;
-        for(int i=0;i<n;i++){
-            mp[arr[i]]++;
-        }
-        vector<pair<int,int>> help;
-        for(auto &it:mp)
-        {
-            help.push_back(it);
-        }
-        sort(help.begin(),help.end(),[](const auto &a,const auto &b){
-            if(a.second==b.second)
+class Solution {
+  public:
+    // Complete this function
+    // Function to sort the array according to frequency of elements.
+    static bool compare(pair<int,int> a,pair<int,int> b){
+        if(a.second==b.second)
             return a.first<b.first;
-            else
+        else
             return a.second>b.second;
-        });
-        for(auto &it: help){
-            v.insert(v.end(),it.second,it.first);
+    }
+    vector<int> sortByFreq(vector<int>& arr) {
+        // Your code here
+        vector<int> ans;
+        map<int,int> mp;
+        for(int num:arr){
+            mp[num]++;
         }
-        return v;
+        vector<pair<int,int>> v;
+        for(auto pair:mp){
+            v.push_back({pair.first,pair.second});
+        }
+        sort(v.begin(),v.end(),compare);
+        for(int i=0;i<v.size();i++){
+            for(int j=0;j<v[i].second;j++){
+                ans.push_back(v[i].first);
+            }
+        }
+        return ans;
     }
 };
 
 //{ Driver Code Starts.
 
 int main() {
-	
-	
-	int t;
-	cin >> t;
-	
-	
-	while(t--){
-	    
-	    
-	    int n;
-	    cin >> n;
-	    
-	    int a[n+1];
-	    
-	    for(int i = 0;i<n;i++){
-	        cin >> a[i];
-	    }
-	    Solution obj;
-	    vector<int> v;
-	    v = obj.sortByFreq(a,n);
-	    for(int i:v)
-	        cout<<i<<" ";
-	    cout << endl;
-	}
-	
-	return 0;
-}
 
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+
+        string input;
+        int num;
+        vector<int> arr;
+
+        getline(cin, input);
+        stringstream s2(input);
+        while (s2 >> num) {
+            arr.push_back(num);
+        }
+        Solution obj;
+        vector<int> v;
+        v = obj.sortByFreq(arr);
+        for (int i : v)
+            cout << i << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
 
 // } Driver Code Ends
