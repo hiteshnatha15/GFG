5331243d0eda92fe9bc3411d4e4293ffde298991
Hitesh Natha <hiteshnatha15@gmail.com>
2026-01-17 12:45:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
+++ Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
@@ -1,77 +1,37 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
-    int kthElement(vector<int>& arr1, vector<int>& arr2, int k) {
+    int kthElement(vector<int> &a, vector<int> &b, int k) {
         // code here
         int i=0;
         int j=0;
-        int n=arr1.size();
-        int m=arr2.size();
-        int count=1;
-        while(i<n && j<m){
-            if(arr1[i]<arr2[j]){
-                if(count==k)
-                    return arr1[i];
-                count++;
+        int cnt=1;
+        while(i<a.size() && j<b.size()){
+            if(a[i]<b[j]){
+                if(cnt==k)
+                    return a[i];
                 i++;
-            }
-            else{
-                if(count==k)
-                    return arr2[j];
-                count++;
+                cnt++;
+
+            } else {
+                if(cnt==k)
+                    return b[j];
                 j++;
+                cnt++;
             }
         }
-        while(i<n){
-            if(count==k)
-                return arr1[i];
+        while(i<a.size()){
+            if(cnt==k)
+                return a[i];
             i++;
-            count++;
+            cnt++;    
         }
-        while(j<m){
-            if(count==k)
-                return arr2[j];
+        
+        while(j<b.size()){
+            if(cnt==k)
+                return b[j];
             j++;
-            count++;
+            cnt++;    
         }
+        return -1;
     }
 };
-
-//{ Driver Code Starts.
-
-// Driver code
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        int n, m, k;
-        cin >> k;
-        cin.ignore();
-        string input;
-        int num;
-        vector<int> arr1, arr2;
-
-        getline(cin, input);
-        stringstream s2(input);
-        while (s2 >> num) {
-            arr1.push_back(num);
-        }
-
-        getline(cin, input);
-        stringstream s3(input);
-        while (s3 >> num) {
-            arr2.push_back(num);
-        }
-
-        Solution ob;
-        cout << ob.kthElement(arr1, arr2, k) << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
