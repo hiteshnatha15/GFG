4a4ca5a270cb5334db4f091d90a3180c873e35f9
Hitesh Natha <hiteshnatha15@gmail.com>
2026-01-17 18:03:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rat in a Maze/rat-in-a-maze.cpp
+++ Difficulty: Medium/Rat in a Maze/rat-in-a-maze.cpp
@@ -1,32 +1,33 @@
 class Solution {
   public:
-    vector<string> ratInMaze(vector<vector<int>>& maze) {
-        // code here
-        vector<string> ans;
-        vector<vector<int>> visited(maze.size(), vector<int>(maze[0].size(), 0));
-        helper(maze,visited,ans,"",0,0);
-        sort(ans.begin(),ans.end());
-        return ans;
-    }
-    void helper(vector<vector<int>>& maze, vector<vector<int>>& visited,vector<string>& ans, string temp, int i, int j){
-        if(i==maze.size()-1 && j==maze.size()-1){
-            ans.push_back(temp);
-            return;
-        }
-        visited[i][j]=1;
-        if(isSafe(maze,visited,i+1,j))
-            helper(maze,visited,ans,temp+"D",i+1,j);
-        if(isSafe(maze,visited,i,j+1))
-            helper(maze,visited,ans,temp+"R",i,j+1);
-        if(isSafe(maze,visited,i-1,j))
-            helper(maze,visited,ans,temp+"U",i-1,j);
-        if(isSafe(maze,visited,i,j-1))
-            helper(maze,visited,ans,temp+"L",i,j-1);
-        visited[i][j]=0;
-    }
     bool isSafe(vector<vector<int>>& maze, vector<vector<int>>& visited, int i, int j){
-        if(i<maze.size() && j < maze.size() && maze[i][j]==1 && visited[i][j]!=1 && i>=0 && j>=0)
+        if (i >= 0 && j >= 0 && i < maze.size() && j < maze.size() && visited[i][j] != 1 && maze[i][j] == 1)
             return true;
         return false;
+    }
+    void helper(vector<vector<int>>& maze, vector<vector<int>>& visited,
+                string temp,vector<string>& ans, int i, int j) {
+                    if(i==maze.size()-1 && j==maze.size()-1){
+                        ans.push_back(temp);
+                        return;
+                    }
+                    
+                    visited[i][j]=1;
+if (isSafe(maze, visited, i + 1, j))
+            helper(maze, visited, temp + 'D', ans, i + 1, j);
+        if (isSafe(maze, visited, i, j - 1))
+            helper(maze, visited, temp + 'L', ans, i, j - 1);
+        if (isSafe(maze, visited, i, j + 1))
+            helper(maze, visited, temp + 'R', ans, i, j + 1);
+        if (isSafe(maze, visited, i - 1, j))
+            helper(maze, visited, temp + 'U', ans, i - 1, j);
+                    visited[i][j]=0;
+                }
+    vector<string> ratInMaze(vector<vector<int>>& maze) {
+        // code here
+        vector<vector<int>> visited(maze.size(), vector<int>(maze.size(),0));
+        vector<string> ans;
+        helper(maze, visited,"",ans, 0, 0);
+        return ans;
     }
 };
