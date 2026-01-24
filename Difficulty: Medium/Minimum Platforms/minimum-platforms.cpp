e3f431a7a539d28623cac567727fd91e2297b03a
Hitesh Natha <hiteshnatha15@gmail.com>
2026-01-24 16:24:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Platforms/minimum-platforms.cpp
+++ Difficulty: Medium/Minimum Platforms/minimum-platforms.cpp
@@ -4,19 +4,20 @@ class Solution {
         // code here
         sort(arr.begin(),arr.end());
         sort(dep.begin(),dep.end());
-        int ans = 0;
-        int temp = 0;
         int i=0;
         int j=0;
+        int ans=0;
+        int temp=0;
+        
         while(i<arr.size() && j<dep.size()){
             if(arr[i]<=dep[j]){
                 i++;
                 temp++;
-            } else {
-                temp--;
+                ans=max(ans,temp);
+            }else{
                 j++;
+                temp--;
             }
-            ans=max(temp,ans);
         }
         return ans;
     }
