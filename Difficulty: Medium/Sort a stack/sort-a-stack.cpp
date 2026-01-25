53ae2ffdd56944127acf160248c0fddc25ba7da7
Hitesh Natha <hiteshnatha15@gmail.com>
2026-01-25 16:37:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort a stack/sort-a-stack.cpp
+++ Difficulty: Medium/Sort a stack/sort-a-stack.cpp
@@ -1,16 +1,5 @@
 class Solution {
   public:
-   void helper(stack<int> &st, int temp){
-       if(st.empty() || st.top()<=temp){
-           st.push(temp);
-           return;
-       }
-       int top = st.top();
-       st.pop();
-       helper(st,temp);
-       st.push(top);
-       return;
-   }
     void sortStack(stack<int> &st) {
         // code here
         if(!st.empty()){
@@ -19,5 +8,17 @@ class Solution {
             sortStack(st);
             helper(st,temp);
         }
+        return;
+    }
+    void helper(stack<int>& st, int temp){
+        if(st.empty() || st.top() <= temp){
+            st.push(temp);
+            return;
+        }
+        int num = st.top();
+        st.pop();
+        helper(st,temp);
+        st.push(num);
+        return;
     }
 };
