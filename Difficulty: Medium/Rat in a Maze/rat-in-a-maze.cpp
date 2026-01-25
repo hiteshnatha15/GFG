4e39fd8e8058c1759e7e41890ada3ff00b439f51
Hitesh Natha <hiteshnatha15@gmail.com>
2026-01-25 10:53:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rat in a Maze/rat-in-a-maze.cpp
+++ Difficulty: Medium/Rat in a Maze/rat-in-a-maze.cpp
@@ -1,33 +1,31 @@
 class Solution {
   public:
-    bool isSafe(vector<vector<int>>& maze, vector<vector<int>>& visited, int i, int j){
-        if (i >= 0 && j >= 0 && i < maze.size() && j < maze.size() && visited[i][j] != 1 && maze[i][j] == 1)
-            return true;
-        return false;
-    }
-    void helper(vector<vector<int>>& maze, vector<vector<int>>& visited,
-                string temp,vector<string>& ans, int i, int j) {
-                    if(i==maze.size()-1 && j==maze.size()-1){
-                        ans.push_back(temp);
-                        return;
-                    }
-                    
-                    visited[i][j]=1;
-if (isSafe(maze, visited, i + 1, j))
-            helper(maze, visited, temp + 'D', ans, i + 1, j);
-        if (isSafe(maze, visited, i, j - 1))
-            helper(maze, visited, temp + 'L', ans, i, j - 1);
-        if (isSafe(maze, visited, i, j + 1))
-            helper(maze, visited, temp + 'R', ans, i, j + 1);
-        if (isSafe(maze, visited, i - 1, j))
-            helper(maze, visited, temp + 'U', ans, i - 1, j);
-                    visited[i][j]=0;
-                }
     vector<string> ratInMaze(vector<vector<int>>& maze) {
         // code here
-        vector<vector<int>> visited(maze.size(), vector<int>(maze.size(),0));
         vector<string> ans;
-        helper(maze, visited,"",ans, 0, 0);
+        vector<vector<int>> dp(maze.size(),vector<int>(maze.size(),0));
+        helper(maze,ans,dp,"",0,0);
         return ans;
+    }
+    void helper(vector<vector<int>>& maze, vector<string>& ans, vector<vector<int>>& dp, string s,int i,int j){
+        if(i==maze.size()-1 && j==maze.size()-1){
+            ans.push_back(s);
+            return;
+        }
+        dp[i][j] = 1;
+        if(isSafe(maze,dp,i+1,j))
+            helper(maze,ans,dp,s+'D',i+1,j);
+        if(isSafe(maze,dp,i,j-1))
+            helper(maze,ans,dp,s+'L',i,j-1);
+        if(isSafe(maze,dp,i,j+1))
+            helper(maze,ans,dp,s+'R',i,j+1);
+        if(isSafe(maze,dp,i-1,j))
+            helper(maze,ans,dp,s+'U',i-1,j);
+        dp[i][j]=0;
+    }
+    bool isSafe(vector<vector<int>>& maze, vector<vector<int>>& dp,int i,int j){
+        if(i<maze.size() && j<maze.size() && i>=0 && j>=0 && maze[i][j]==1 && dp[i][j]==0)
+            return true;
+        return false;
     }
 };
