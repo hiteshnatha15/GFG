f6723adcbcc64e86c6da0e283a3796732bce7602
Hitesh Natha <hiteshnatha15@gmail.com>
2026-02-09 11:01:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Find Kth Rotation/find-kth-rotation.cpp
+++ Difficulty: Easy/Find Kth Rotation/find-kth-rotation.cpp
@@ -1,59 +1,27 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-
-using namespace std;
-
-
-// } Driver Code Ends
-// User function template for C++
 class Solution {
   public:
     int findKRotation(vector<int> &arr) {
         // Code Here
-        int ans=INT_MAX;
-        int low=0;
-        int high=arr.size()-1;
-        int index=0;
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(arr[low]<=arr[mid]){
-                if(ans>arr[low]){
-                    ans=arr[low];
-                    index=low;
+        int l = 0;
+        int r = arr.size()-1;
+        int ans = INT_MAX;
+        int idx = 0;
+        while(l<=r){
+            int mid = (l+r)/2;
+            if(arr[l]<=arr[mid]){
+                if(ans>arr[l]){
+                    ans = arr[l];
+                    idx = l;
                 }
-                low=mid+1;
-            }
-            else{
+                l = mid + 1;
+            } else {
                 if(ans>arr[mid]){
-                    ans=arr[mid];
-                    index=mid;
+                    ans = arr[mid];
+                    idx = mid;
                 }
-                high=mid-1;
+                r = mid - 1;
             }
         }
-        return index;
+        return idx;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> arr;
-        string input1;
-        getline(cin, input1);
-        stringstream ss1(input1);
-        int number1;
-        while (ss1 >> number1) {
-            arr.push_back(number1);
-        }
-        Solution ob;
-        int res = ob.findKRotation(arr);
-        cout << res << endl;
-    }
-}
-// } Driver Code Ends
