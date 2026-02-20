4cbe82021810e870f115883a9cf920317fd0000b
Hitesh Natha <hiteshnatha15@gmail.com>
2026-02-20 14:36:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/N meetings in one room/n-meetings-in-one-room.java
+++ Difficulty: Easy/N meetings in one room/n-meetings-in-one-room.java
@@ -3,18 +3,17 @@ class Solution {
     // be performed in a meeting room.
     public int maxMeetings(int start[], int end[]) {
         // add your code here
-        List<List<Integer>> meetings = new ArrayList<>();
-        for(int i=0;i<start.length;i++) {
-            meetings.add(Arrays.asList(end[i], start[i]));
+        List<int[]> list = new ArrayList<>();
+        for(int i=0;i<start.length;i++){
+            list.add(new int[]{end[i],start[i]});
         }
-        meetings.sort(Comparator.comparingInt(a -> a.get(0)));    
-        int maxEnd = meetings.get(0).get(0);
+        Collections.sort(list,(a,b)->a[0]-b[0]);
         int ans = 1;
-        for(int i = 1;i<meetings.size();i++){
-            if(maxEnd < meetings.get(i).get(1)) {
+        int maxEnd = list.get(0)[0];
+        for(int i = 1;i<list.size();i++){
+            if(maxEnd < list.get(i)[1]){
+                maxEnd = Math.max(maxEnd,list.get(i)[0]);
                 ans++;
-                maxEnd = Math.max(maxEnd,meetings.get(i).get(0));
-
             }
         }
         return ans;
