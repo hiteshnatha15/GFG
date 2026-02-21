3650f8144adf6eaf0d2d662739476c1bcef49934
Hitesh Natha <hiteshnatha15@gmail.com>
2026-02-21 14:20:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Platforms/minimum-platforms.java
+++ Difficulty: Medium/Minimum Platforms/minimum-platforms.java
@@ -1,20 +1,20 @@
 class Solution {
     public int minPlatform(int arr[], int dep[]) {
         //  code here
-        int i = 0;
-        int j = 0;
         int ans = 0;
-        int temp = 0;
+        int j=0;
+        int i=0;
+        int count = 0;
         Arrays.sort(arr);
         Arrays.sort(dep);
         
         while(i<arr.length && j<arr.length){
             if(arr[i]<=dep[j]){
-                temp++;
-                ans = Math.max(ans,temp);
+                count++;
+                ans=Math.max(ans,count);
                 i++;
-            } else {
-                temp--;
+            } else{
+                count--;
                 j++;
             }
         }
